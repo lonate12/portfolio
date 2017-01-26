1aca842daa5f2ac2141a9e78a606e3d015ca8a31
Luis Rene Onate <lonate12@gmail.com>
2017-01-26 11:42:17 -0500

    Linked the nave links.

--- app/scripts/components/homeContainer.jsx
+++ app/scripts/components/homeContainer.jsx
@@ -6,7 +6,7 @@ var Footer = require('./presentation/footer.jsx');
 var HomeContainer = React.createClass({
   render: function(){
     return(
-      <div className="container">
+      <div className="container" id="home">
         <NavBar />
         <header className="banner row">
           <div className="main-img-container">
@@ -15,9 +15,9 @@ var HomeContainer = React.createClass({
             <i className="chevron fa fa-chevron-down" aria-hidden="true"></i>
           </div>
         </header>
-        <div className="fs-container red row"></div>
-        <div className="fs-container blue row"></div>
-        <div className="fs-container green row"></div>
+        <div id="about" className="fs-container red row"></div>
+        <div id="projects" className="fs-container blue row"></div>
+        <div id="contact" className="fs-container green row"></div>
         <Footer />
       </div>
     );
