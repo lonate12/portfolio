0952fed3bc58f7ea54b03070be3b5b387d4cb775
Luis Rene Onate <lonate12@gmail.com>
2017-01-26 11:07:06 -0500

    Added some placeholder text and divs.

--- app/scripts/components/homeContainer.jsx
+++ app/scripts/components/homeContainer.jsx
@@ -10,10 +10,14 @@ var HomeContainer = React.createClass({
         <NavBar />
         <header className="banner row">
           <div className="main-img-container">
+            <img src="./images/lro-logo-white.svg" className="logo-large"></img>
+            <h2 className="tagline">Not your average front-end developer!</h2>
             <i className="chevron fa fa-chevron-down" aria-hidden="true"></i>
           </div>
         </header>
-
+        <div className="fs-container red row"></div>
+        <div className="fs-container blue row"></div>
+        <div className="fs-container green row"></div>
         <Footer />
       </div>
     );
