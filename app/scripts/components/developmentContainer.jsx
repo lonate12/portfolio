a478a7e7b918646e764eb998808ef4348eb0f5aa
Luis Rene Onate <lonate12@gmail.com>
2017-02-03 18:17:03 -0800

    Updated content for projects info.

--- app/scripts/components/developmentContainer.jsx
+++ app/scripts/components/developmentContainer.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var $ = require('jquery');
+var descriptions = require('../projectDescriptions.js');
 
 var DevelopmentContainer = React.createClass({
   noScroll: function(e){
@@ -14,37 +15,72 @@ var DevelopmentContainer = React.createClass({
     });
   },
   render: function(){
+    var techsStarWars = descriptions.starWars.tech.map(function(tech){
+      return(
+        <p key={tech} className="tech">{tech}</p>
+      );
+    });
+
+    var techsGitHub = descriptions.gitHub.tech.map(function(tech){
+      return(
+        <p key={tech} className="tech">{tech}</p>
+      );
+    });
+
+    var techsTheStandings = descriptions.theStandings.tech.map(function(tech){
+      return(
+        <p key={tech} className="tech">{tech}</p>
+      );
+    });
+
     return (
       <div id="projects" className="fs-container row">
         <div className="col-md-6 project-image-container">
-          <h1 className="text-center white">Previous Projects</h1>
+          <h1 className="text-center projects-header">Previous</h1>
+          <h1 className="text-center projects-header">Projects</h1>
         </div>
         <div className="col-md-6 project-description-container">
           <div className="col-md-12 project">
             <div className="col-md-4 project-thumbnail star-wars-wg-thumbnail"></div>
-            <div className="col-md-8 project-description" onScroll={this.noScroll}>
-              <h2 className="">Star Wars Word Game</h2>
-              <p className="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="col-md-8 project-description star-wars" onScroll={this.noScroll}>
+              <h2>{descriptions.starWars.name}</h2>
+              <p>{descriptions.starWars.description1}</p>
+              <p>{descriptions.starWars.description2}</p>
+              <h3 className="technologies-used text-center">Technologies Used</h3>
+              <div className="tech-div">
+                {techsStarWars}
+              </div>
               <a className="col-md-4 col-md-offset-1 btn btn-success" href="">Source Code</a>
-              <a className="col-md-4 col-md-offset-1 btn btn-success" href="">Live Project</a>
+              <a className="col-md-4 col-md-offset-2 btn btn-success" href="">Live Project</a>
             </div>
           </div>
           <div className="col-md-12 project">
             <div className="col-md-4 project-thumbnail github-replica-thumbnail"></div>
-            <div className="col-md-8 project-description" onScroll={this.noScroll}>
-              <h2 className="">Github Replica</h2>
-              <p className="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="col-md-8 project-description github" onScroll={this.noScroll}>
+              <h2>{descriptions.gitHub.name}</h2>
+              <p>{descriptions.gitHub.description1}</p>
+              <p>{descriptions.gitHub.description2}</p>
+              <h3 className="technologies-used text-center">Technologies Used</h3>
+              <div className="tech-div">
+                {techsGitHub}
+              </div>
               <a className="col-md-4 col-md-offset-1 btn btn-success" href="">Source Code</a>
-              <a className="col-md-4 col-md-offset-1 btn btn-success" href="">Live Project</a>
+              <a className="col-md-4 col-md-offset-2 btn btn-success" href="">Live Project</a>
             </div>
           </div>
           <div className="col-md-12 project">
             <div className="col-md-4 project-thumbnail the-standings-thumbnail"></div>
-            <div className="col-md-8 project-description" onScroll={this.noScroll}>
-              <h2 className="">The Standings</h2>
-              <p className="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="col-md-8 project-description the-standings" onScroll={this.noScroll}>
+              <h2>{descriptions.theStandings.name}</h2>
+              <p>{descriptions.theStandings.description1}</p>
+              <p>{descriptions.theStandings.description2}</p>
+              <p>{descriptions.theStandings.description3}</p>
+              <h3 className="technologies-used text-center">Technologies Used</h3>
+              <div className="tech-div">
+                {techsTheStandings}
+              </div>
               <a className="col-md-4 col-md-offset-1 btn btn-success" href="">Source Code</a>
-              <a className="col-md-4 col-md-offset-1 btn btn-success" href="">Live Project</a>
+              <a className="col-md-4 col-md-offset-2 btn btn-success" href="">Live Project</a>
             </div>
           </div>
         </div>
---
+++ app/scripts/projectDescriptions.js
@@ -0,0 +1,43 @@
+var starWars = {
+  name: 'Star Wars Word Game',
+  description1: "For one of my first JavaScript assignments I was instructed to "+
+  "create a game where a random word was to be drawn from a word bank and the player "+
+  "had to guess the word.",
+  description2: "Naturally, this soon evolved into a Star Wars themed "+
+  "word game complete with the Star Wars theme song, laser sounds, and guest apperances "+
+  "from Darth Vader and the Emperor!",
+  tech: ["HTML5", "CSS3", "JavaScript", "Git", "GitHub"]
+};
+
+var gitHub = {
+  name: "GitHub Replica",
+  description1: "Developers far and wide are familiar with GitHub. This project showcases " +
+  "my abilities to replicate another person's design (i.e. working with a designer's comp) and also" +
+  "my ability to make API request and handle the response JSON data.",
+  description2: "In this project I also used Handlebars.js to make stamps of repeating elements " +
+  "and Git/GitHub for source control.",
+  tech: ["HTML5", "CSS3", "JavaScript", "jQuery", "Handlebars.js", "GitHub API", "Git", "GitHub"]
+};
+
+var theStandings = {
+  name: "The Standings",
+  description1: "For my final project I called on my experiences while running the " +
+  "Clemson Club Soccer team. We hosted one of the largest club soccer in the South East " +
+  "every semester. We registered teams through snail mail, kept teams updated with the standings " +
+  "displayed on a piece of poster board on a single field, and didn't really have a way " +
+  "of calculating the points other than by hand.",
+  description2: "Enter my inspiration for creating 'The Standings.' It's a web app that makes " +
+  "use of what I learned at the Iron Yard. It allows a master admin to create a tournament and " +
+  "manage the tournaments from a central dashboard. Additionally, visiting teams can register online " +
+  "and keep track of their teams standings within their group as well as the tournament as a whole. " +
+  "Guests of the teams can also keep track of their team's progress, scores, and game locations.",
+  description3: "Overall, 'The Standings' is my first robust web application that will be tested " +
+  "and used by the Clemson Club Soccer Team.",
+  tech: ["HTML5", "CSS3", "JavaScript", "React.js", "Backbone.js", "SASS", "Bootstrap", "Git", "GitHub"]
+};
+
+module.exports = {
+  starWars: starWars,
+  gitHub: gitHub,
+  theStandings: theStandings
+};
